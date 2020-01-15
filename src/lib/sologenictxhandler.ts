87bbdee1028a785935578e12db1b57fc97a2a835
Reza Bashash <rezabashash@Rezas-MacBook-Pro.local>
2020-01-14 18:06:19 -0800

    output 1000 error

--- src/lib/sologenictxhandler.ts
+++ src/lib/sologenictxhandler.ts
@@ -114,6 +114,7 @@ export class SologenicTxHandler extends EventEmitter {
         precision: 64
       });
     } catch (error) {
+      console.log(error);
       throw new SologenicError('1001');
     }
   }
