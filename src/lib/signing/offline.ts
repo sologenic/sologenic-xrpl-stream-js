b025b1780363b28cecc414046b4dae34f3f180b6
Russell McConnachie <pmcconna@cisco.com>
2020-04-25 12:39:52 -0700

    Revert "Ensure that the tfFullyCanonicalSig is always enabled as per the https://xrpl.org/known-amendments.html\#requirefullycanonicalsig amendment."
    This reverts commit 1ebb2d772d1fe7cb28da587920e6f7e5b14e7721.

--- src/lib/signing/offline.ts
+++ src/lib/signing/offline.ts
@@ -1,4 +1,5 @@
 import { RippleAPI } from 'ripple-lib';
+import { SologenicError } from '../error';
 import * as SologenicTypes from '../../types';
 
 import XrplAccount from '../account';
@@ -29,16 +30,6 @@ export class OfflineSigner extends SologenicTxSigner
       if (txJson.TransactionMetadata) {
         delete txJson.TransactionMetadata;
       }
-
-      if (typeof txJson.Flags === 'undefined')
-        txJson.Flags = 0;
-
-      if ((txJson.Flags & 0x80000000) === 0) {
-        txJson.Flags |= 0x80000000;
-      }
-
-      // Convert tx.Flags to an unsigned
-      txJson.Flags = txJson.Flags >>> 0;
 
       const signedTx: SologenicTypes.SignedTx = this.rippleApi.sign(
         JSON.stringify(txJson),
--- src/tests/signing/offline.spec.ts
+++ src/tests/signing/offline.spec.ts
@@ -2,7 +2,7 @@ import anyTest, {TestInterface} from 'ava';
 
 import { OfflineSigner } from '../../lib/signing/offline';
 import XrplAccount from '../../lib/account';
-import * as SologenicTypes from '../../types';
+import { SignedTx } from '../../types';
 
 const binaryCodec = require('ripple-binary-codec');
 
@@ -25,37 +25,11 @@ test.serial("sign a transaction and verify the signature", async t => {
   const signer = new OfflineSigner({});
 
   // Encode transaction and sign
-  const signedTx: SologenicTypes.SignedTx = await signer.sign(txJson, "1234", account, {});
+  const signedTx: SignedTx = await signer.sign(txJson, "1234", account, {});
 
   // Decode the transaction
   const decodedTransaction = binaryCodec.decode(signedTx.signedTransaction);
 
   t.is(decodedTransaction.TransactionType, txJson.TransactionType);
   t.is(decodedTransaction.SetFlag, txJson.SetFlag);
-});
-
-test.serial("sign a transaction and verify the flags", async t => {
-  const account = new XrplAccount(
-    "rE3uRzyUuKCFGuuUSKPPz7MeLcbckeuczu",
-    "snevTM71p4xj5ZQcnBvpHeb9P3Umn");
-
-  const txJson = {
-    Account: account.getAddress(),
-    TransactionType: 'AccountSet',
-    SetFlag: 1
-  };
-
-  const signer = new OfflineSigner({});
-
-  // Encode transaction and sign
-  const signedTx: SologenicTypes.SignedTx = await signer.sign(txJson, "1234", account, {});
-
-  // Decode the transaction
-  const decodedTransaction = binaryCodec.decode(signedTx.signedTransaction);
-
-  t.is(decodedTransaction.TransactionType, txJson.TransactionType);
-  t.is(decodedTransaction.SetFlag, txJson.SetFlag);
-
-  // Verify the tfFullyCanonicalSig is set.
-  t.is((decodedTransaction.Flags & 0x80000000) >>> 0, 2147483648);
 });
