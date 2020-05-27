b1a3026689c1b666f04326a917a176134e90a51a
Russell McConnachie <russell@mcconnachie.ca>
2020-05-27 07:32:52 -0700

    Fix failed tx event, and add a test for verifying transactions with a keypair.

--- src/tests/compatibility.spec.ts
+++ src/tests/compatibility.spec.ts
@@ -4,7 +4,7 @@ import { SologenicTxHandler } from '../lib/txhandler';
 import XrplAccount, { XrplAddressException, XrplSecretException, XrplKeypairException, XrplKeypairOrSecretMissingException } from '../lib/account';
 import { generateSeed, deriveAddress, deriveKeypair } from 'ripple-keypairs';
 
-test.serial('validate backwards compatibility', async function(t) {
+test.serial('validate backwards compatibility, using secret wallets', async function(t) {
   const seed = 'ssH5SSKYvHBynnrYoCnmvsbxrNGEv';
   const keypair = deriveKeypair(seed);
   const address = deriveAddress(keypair.publicKey);
@@ -53,7 +53,7 @@ test.serial('validate backwards compatibility', async function(t) {
     t.is(sologenic.getAccount().getAddress(), xrplAccount.getAddress());
     t.is(sologenic.getAccount().getSecret(), xrplAccount.getSecret());
 
-    const tx = sologenic.submit({
+    await sologenic.submit({
       TransactionType: 'Payment',
       Account: 'rNbe8nh1K6nDC5XNsdAzHMtgYDXHZB486G',
       Destination: 'rUwty6Pf4gzUmCLVuKwrRWPYaUiUiku8Rg',
@@ -62,27 +62,58 @@ test.serial('validate backwards compatibility', async function(t) {
         issuer: 'rNbe8nh1K6nDC5XNsdAzHMtgYDXHZB486G',
         value: '100000000'
       }
-    });
-
-    // Events have their own types now.
-    tx.events.on('queued', (event: SologenicTypes.QueuedEvent) => {
-      console.log('TX QUEUED: ', event);
-    }).on('dispatched', (event: SologenicTypes.DispatchedEvent) => {
-      console.log('TX DISPATCHED:', event);
-    }).on('requeued', (event: SologenicTypes.RequeuedEvent) => {
-      console.log('TX REQUEUED:', event);
-    }).on('warning', (event: SologenicTypes.WarningEvent) => {
-      console.log('TX WARNING:', event);
-    }).on('validated', (event: SologenicTypes.ValidatedEvent) => {
-      console.log('TX VALIDATED:', event);
-    }).on('failed', (event: SologenicTypes.FailedEvent) => {
-      console.log('TX FAILED:', event);
-    });
-
-    await tx.promise;
+    }).promise;
 
   } catch (error) {
-    console.log('Error:', error);
+    t.fail(error);
+  }
+});
+
+test.serial('validate backwards compatibility, using keypair', async function(t) {
+  const seed = 'ssH5SSKYvHBynnrYoCnmvsbxrNGEv';
+  const keypair = deriveKeypair(seed);
+  const address = deriveAddress(keypair.publicKey);
+
+  try {
+    const sologenic = await new SologenicTxHandler(
+      // RippleAPI Options
+      {
+        server: 'wss://testnet.xrpl-labs.com', // Kudos to Wietse Wind
+      },
+      // Sologenic Options, hash or redis
+      {
+        clearCache: true,
+        queueType: "hash",
+        hash: {}
+      }
+    ).connect();
+
+    await sologenic.setAccount(<SologenicTypes.Account>{
+      address: 'rNbe8nh1K6nDC5XNsdAzHMtgYDXHZB486G',
+      keypair: {
+        publicKey: keypair.publicKey,
+        privateKey: keypair.privateKey
+      }
+    });
+
+    t.is(sologenic.getAccount().getAddress(), address);
+    t.is(sologenic.getAccount().getSecret(), undefined);
+    t.is(sologenic.getAccount().getKeypair().publicKey, keypair.publicKey);
+    t.is(sologenic.getAccount().getKeypair().privateKey, keypair.privateKey);
+
+    await sologenic.submit({
+      TransactionType: 'Payment',
+      Account: 'rNbe8nh1K6nDC5XNsdAzHMtgYDXHZB486G',
+      Destination: 'rUwty6Pf4gzUmCLVuKwrRWPYaUiUiku8Rg',
+      Amount: {
+        currency: '534F4C4F00000000000000000000000000000000',
+        issuer: 'rNbe8nh1K6nDC5XNsdAzHMtgYDXHZB486G',
+        value: '100000000'
+      }
+    }).promise;
+
+  } catch (error) {
+    t.fail(error);
   }
 });
 
